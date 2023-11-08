4f49e9ccfbf6421e9fdbd486e8577b7d74ee004c
ArmandoCruzV <hao_2-d@hotmail.com>
2023-11-07 22:18:09 -0600

    Rediseñando home

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,30 +1,38 @@
 import React from "react";
 import { Slider } from "../../Components/Slider";
 import icono from '../../assets/icono-MIR.png';
+import molinos from '../../assets/molinos.png';
+import planeta from '../../assets/planeta.png';
 import './style.css';
 
 function Home(){
 
     return(
         <div className="home">
+
+            <img src={molinos} className="molinos"/>
+            <img src={planeta} className="planeta"/>
+
             <div className="home-figure--title">
                 <span id="title">Cuchillas y Molinos</span>
-                <span id="title-reciclaje">Reciclaje y venta</span>
-                <span id="title-reciclaje2">de plástico molido</span>
+                <span id="title-reciclaje">Reciclaje y venta de</span>
+                <span id="title-reciclaje2"> plástico molido</span>
                 <img id="title-reciclaje3" src={icono}></img>
-                <span className="subtitle">Venta de plástico reciclado molido, cuchillas y equipos para la industria misma.</span>
+                <div></div>
+                <p className="subtitle">Venta de plástico reciclado molido, cuchillas y equipos para la industria misma.</p>
             </div>
 
             <div className="home-figure">
-                <Slider />
+                <Slider className="figure"/>
             </div>
 
             <div className="home-span">
             <span >
-                <p>Fabricantes de cuchillas, compra venta de molinos 
+                <p>Venta de Polipropileno (PP) y Polietileno de alta densidad (HDPE) entre otros tipos de plásticos,
+                     fabricantes de cuchillas, compra venta de molinos 
                     de plástico y equipo periférico como son compactadoras,
-                     paletizadoras, pigmenta doras y reciclado, desde hace 
-                     más de 18 años, creada con la finalidad de cubrir las
+                     paletizadoras, pigmentadoras y reciclado, 
+                        creada con la finalidad de cubrir las
                       necesidades del mercado de la industria del plástico
                        para atender las demandas más exigentes de empresas 
                     que se dedican a la transformación del plástico.</p>
